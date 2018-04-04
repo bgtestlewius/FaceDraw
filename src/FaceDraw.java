81763805de47a2e083618e29bebc5dccce62e53b
bgtestlewius <williampgillespie@lewisu.edu>
2018-04-03 23:42:40 -0500

    Add FaceDraw.Java

---
+++ src/FaceDraw.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author xwxg008
+ *
+ */
+public class FaceDraw {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
